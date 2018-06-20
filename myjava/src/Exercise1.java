6438462e05bfbfb34eb0266cc996466f84907f78
choongang402 <choongang402@ca>
2018-06-20 16:13:37 +0900

    exercises chapter1

---
+++ myjava/src/Exercise1.java
@@ -0,0 +1,34 @@
+
+public class Exercise1 {
+	public static void main(String[] args) {
+		int modelName;
+//		int class;
+//		int 6hour;
+		int $value;
+		int _age;
+//		int int;
+		
+		
+		char charValue = 'A';
+		short shortValue = charValue;
+		
+		int intValue = 10;
+		double var = (double) intValue;
+		String strValue = "A";
+		char var = (char) strValue;
+		
+		
+		char var3 = ' ';
+		
+		byte byteValue = 10;
+		byte result = (byte) (byteValue + byteValue);
+	}
+}
+
+
+
+
+
+
+
+
