1666f2ac5d730b6e63b6d3b73a68047d1d6a0b27
choongang402 <choongang402@ca>
2018-06-29 10:17:00 +0900

    chapter 06 exercises 19, 20

---
+++ myjava/src/chapter06/excercise19/Account.java
@@ -0,0 +1,19 @@
+package chapter06.excercise19;
+
+public class Account {
+    private int balance;
+    private static final int MIN_BALANCE = 0;
+    private static final int MAX_BALANCE = 1_000_000;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        if (balance >= MIN_BALANCE && balance <= MAX_BALANCE) {
+            this.balance = balance;
+        }
+    }
+    
+    
+}
