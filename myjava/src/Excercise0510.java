c7e068870cc3e352eacb67fdfd63a61c9c0c0ecd
choongang402 <choongang402@ca>
2018-06-29 16:36:00 +0900

    code test

--- myjava/src/Excercise0510.java
+++ myjava/src/Excercise0510.java
@@ -5,13 +5,13 @@ public class Excercise0510 {
     public static void main(String[] args) {
         int[] array = new int[10];
         for (int i = 0; i < array.length; i++) {
-            array[i] = new Random().nextInt(13);
+            array[i] = new Random().nextInt(13) + 1;
         }
         
         Arrays.sort(array);
         
         System.out.println(Arrays.toString(array));
-        // 1~10의 임의의 값이 정렬된 배열에서
+        // 1~13의 임의의 값이 정렬된 배열에서
         // 더해서 13이 되는 쌍이 있는 지 확인하는
         // 코드를 작성해보세요.
         // 배열을 한번만 탐색해서 완성해보도록 하세요.
